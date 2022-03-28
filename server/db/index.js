4be881e9f1d6847b60f49c67755b15d86a5c4576
patrycja panasiuk <panasiuk.patrycja@gmail.com>
2022-03-28 17:42:28 +0200

    change branching for dbs, now only working with dev db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,12 @@
 const postgres = require('postgres');
 require('dotenv').config();
 
-const sqlUrl = process.env.NODE_ENV === 'production' ? process.env.SQL_URL_PROD : process.env.SQL_URL_DEV;
+// const sqlUrl = process.env.NODE_ENV === 'production'
+//  ? process.env.SQL_URL_PROD
+//  : process.env.SQL_URL_DEV;
+
+const sqlUrl = process.env.SQL_URL_DEV;
+
 const sql = postgres(sqlUrl);
 
 module.exports = sql;
