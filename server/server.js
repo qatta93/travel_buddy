28e1bf5044ac4d778ba23234d569cbe598205ef5
patrycja panasiuk <panasiuk.patrycja@gmail.com>
2022-03-28 09:10:11 +0200

    Server setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5500;
+
+const app = express();
+app.use(cors());
+
+app.get('/api', (req,res) => {
+  res.json({message: 'Hello World'});
+});
+
+app.listen(port, () => console.log(`I am listening on port ${port}`));
