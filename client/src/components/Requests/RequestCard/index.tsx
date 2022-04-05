693605f7a722deab37d23ab8a0ea5bc273aa6e32
patrycja panasiuk <panasiuk.patrycja@gmail.com>
2022-04-05 10:11:42 +0200

    cancel detele buttons added

--- client/src/components/Requests/RequestCard/index.tsx
+++ client/src/components/Requests/RequestCard/index.tsx
@@ -42,11 +42,31 @@ const RequestCard = ({ request }:RequestCardProps) => {
   return (
     <article className={`request-card request-card--${status}`}>
       <header className={`request-card__header request-card__header--${status}`}>
-        <h1 className="request-card__title">
-          {tripCountries}
-          {', '}
-          {tripDate}
-        </h1>
+        <div className="request-card__wrapper">
+          <h1 className="request-card__title">
+            {tripCountries}
+            {', '}
+            {tripDate}
+          </h1>
+          {status === 'accepted' ? (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 request-card__cancel" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+            </svg>
+          )
+            : null}
+          {status === 'pending' ? (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 request-card__cancel" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+            </svg>
+          )
+            : null}
+          {status === 'rejected' ? (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 request-card__delete" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+            </svg>
+          )
+            : null}
+        </div>
         <p className="request-card__name">{tripHostName}</p>
       </header>
       <div className="request-card__buttons">
