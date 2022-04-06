2e27216d356a6c22bd573ae2eed16a1d0a703b98
Alejandro A. Aburto Salazar <75444835+aburto22@users.noreply.github.com>
2022-04-06 09:55:44 +0200

    fixed layout user page and included trips list

--- client/src/components/DisplayUser/index.tsx
+++ client/src/components/DisplayUser/index.tsx
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { IUser } from '../../types';
+import { IUser, ITrip } from '../../types';
 import { fetchApi } from '../../helpers/api';
 import MainHeader from '../MainHeader';
 import DisplayCard from './DisplayCard';
+import Trips from '../Trips';
 import './style.css';
 
 export const DisplayUser = () => {
   const [userDetails, setUserDetails] = useState<IUser | null>(null);
+  const [userTrips, setUserTrips] = useState<ITrip[]>([]);
   const { id } = useParams();
 
   useEffect(() => {
-    const getUserDetails = async () => {
-      const data = await fetchApi<IUser>(`/api/users/${id}`);
-      if (data.status === 'error') {
-        console.error(data.message);
-        return;
+    const getUserDetailsAndTrips = async () => {
+      const [userDetailsData, tripsData] = await Promise.all([
+        fetchApi<IUser>(`/api/users/${id}`),
+        fetchApi<ITrip[]>('/api/trips'),
+      ]);
+
+      if (tripsData.status === 'success') {
+        const userTripsData = tripsData.data.filter((t) => t.author.id === Number(id));
+        setUserTrips(userTripsData);
       }
-      setUserDetails(data.data);
+
+      if (userDetailsData.status === 'success') {
+        setUserDetails(userDetailsData.data);
+      }
+
+      if (userDetailsData.status === 'error') {
+        console.log(userDetailsData.message);
+      }
+
+      if (tripsData.status === 'error') {
+        console.log(tripsData.message);
+      }
     };
 
-    getUserDetails();
+    getUserDetailsAndTrips();
   }, []);
 
   return (
@@ -35,6 +52,11 @@ export const DisplayUser = () => {
       {userDetails && (
         <section className="display-card">
           <DisplayCard user={userDetails} />
+        </section>
+      )}
+      {userTrips.length > 0 && (
+        <section className="display-user__trips">
+          <Trips trips={userTrips} />
         </section>
       )}
       <img className="display-user__img" src="/images/bg_desktop.png" alt="car" />
